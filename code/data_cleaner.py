6c5dfbc7242d2af52ecfbae403179f96045d6570
galongoria <97994476+galongoria@users.noreply.github.com>
2022-03-23 11:24:35 -0500

    Updated code for cleaner

--- code/data_cleaner.py
+++ code/data_cleaner.py
@@ -1,9 +1,12 @@
+import os
+import pandas as pd
+
 IN_FILE_PATH = os.path.join("../data", "invoices_xl")
-OUT_FILE = "clean_data_322.csv"
+OUT_FILE = "clean_data.csv"
 OUT_DIR = os.path.join("../data")
 OUT_PATH = os.path.join(OUT_DIR, OUT_FILE)
 
-def data_cleaner(directory):
+def invoice_to_df(directory):
     df = pd.DataFrame()
     i = 0
     read = True    
@@ -22,12 +25,21 @@ def data_cleaner(directory):
         i += 1
     return df
 
+def clean_df(df):
+    read = True
+    i = 0
+    for index, row in df.iterrows():
+        if read == True:
+            df.loc[i, "daily_ct"] = str(row[7])[0:2]
+            df.loc[i, "day"] = str(row[7])[6:]
+            df.loc[i, "month"] = str(row[7])[4:6]
+            df.loc[i, "year"] = str(row[7])[2:4]
+            i +=1
+    return df
 
 
 if __name__ == "__main__":
     os.makedirs(OUT_DIR, exist_ok=True)
-    clean_data = data_cleaner(IN_FILE_PATH)
-    clean_data.to_csv(OUT_PATH, index = False)
-    
-
-
+    clean_data = invoice_to_df(IN_FILE_PATH)
+    clean_df = clean_df(clean_data)
+    clean_df.to_csv(OUT_PATH, index = False)
